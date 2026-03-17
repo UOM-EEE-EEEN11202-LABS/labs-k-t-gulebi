47077d7e88f9eeb8cd366dfcaff99f66248547db
gulebi <gleb.ivanov.tech@gmail.com>
2026-03-17 13:29:36 +0000

    Assignment K done

--- lab-k/assignment_k.c
+++ lab-k/assignment_k.c
@@ -1,11 +1,24 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <stdio.h>
 
 int main(void) {
+  // define the email
+  static const char email[] = "gleb.ivanov@student.manchester.ac.uk";
   double height = 0;
+  double radius = 2.2;
+  double volume = 0;
   printf("Enter height: ");
   scanf("%lf", &height);
 
   // Your code in this section
+
+  if (height < 0) {
+    printf("Error.\n");
+    return 1; // Exit with an error code
+  }
+
+  volume = M_PI * pow(radius, 2) * height;
 
   // Do not edit below here
   printf("%s\n", email);
